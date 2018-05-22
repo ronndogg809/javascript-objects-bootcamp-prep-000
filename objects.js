30339fee9a6f6b3ec362bf8ce6da4f4613dc1ea8
Ronald nunez <ronaldcuello11@gmail.com>
2018-05-22 05:09:00 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -6,5 +6,7 @@ function updatePlaylist(playlist, artistName, songtitle){
 playlist[artistName] = 'songtitle';
 return playlist
 }
-function removeFromPlaylist(playlist, aristNames)
-return 
+function removeFromPlaylist(playlist, aristNames){
+return playlist.assign({})
+}
+ 
